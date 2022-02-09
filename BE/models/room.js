667176725ea470f5bbb684863dcc2d59bbe4a235
dranogajec <daniel.ranogajec@sartura.hr>
2022-02-09 20:16:07 +0100

    changed room model fit to store users, added user endpoints

--- BE/models/room.js
+++ BE/models/room.js
@@ -1,16 +1,32 @@
 const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 
+const userSchema = new Schema({
+    user_id: {
+        type: String
+    },
+    firstname: {
+        type: String
+    },
+    lastname: {
+        type: String
+    },
+	picture: {
+		type: String
+	}
+}, { _id : false })
+
 const roomSchema = new Schema({
 	id: {
 		type: String,
 		required: true,
 		unique: true
 	},
-	owner_id: {
-		type: String,
+	owner: {
+		type: userSchema,
 		required: true
 	},
+	users: [userSchema],
 	expires_at: {
 		type: String
 	}
--- BE/routes/index.js
+++ BE/routes/index.js
@@ -3,18 +3,24 @@ var router = express.Router();
 const Room = require('../models/room')
 const fetch = require("node-fetch");
 
-
+//
 //TODO: make trigger to delete room when expires_at comes
+//
+//
+//
+
 router.get('/room/:roomId', function(req, res, next) {
   Room.find({"id": req.params.roomId}).then((result) => {
 		res.send(result)
 	}).catch((err) => console.log(err))
 });
 
+//parameters sent: id, owner as { user_id: "", firstname: "", lastname: "", picture: "" }, expires_at
 router.post('/addRoom', function(req, res, next) {
 	const room = new Room({
 		id: req.body.id,
-		owner_id: req.body.owner_id,
+		owner: req.body.owner,
+		users: [],
 		expires_at: req.body.expires_at
 	})
 
@@ -23,6 +29,19 @@ router.post('/addRoom', function(req, res, next) {
 	}).catch((err) => console.log(err))
 });
 
+//parameters sent: user as { user_id: "", firstname: "", lastname: "", picture: "" }
+router.post('/room/:roomId/addUser', function(req, res, next) {
+	Room.updateOne({"id": req.params.roomId}, {$push: {"users": req.body.user}}).then((result) => {
+		res.send(result)
+	}).catch((err) => console.log(err))
+});
+
+//parameters sent: user as { user_id: "", firstname: "", lastname: "", picture: "" }
+router.post('/room/:roomId/removeUser', function(req, res, next) {
+	Room.updateOne({"id": req.params.roomId}, {$pull: {"users": req.body.user}}).then((result) => {
+		res.send(result)
+	}).catch((err) => console.log(err))
+});
 
 
 
