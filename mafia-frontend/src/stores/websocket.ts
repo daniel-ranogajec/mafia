b2652ea705b812e3896dae0514e4da3dee68fa09
AntonioNeznamProgramirat <antonio.pesic@boostinc.com>
2024-08-07 14:17:47 +0200

    leaving game removes the player

--- mafia-frontend/src/stores/websocket.ts
+++ mafia-frontend/src/stores/websocket.ts
@@ -1,51 +1,60 @@
-import { ref, computed } from 'vue';
-import { defineStore } from 'pinia';
+import { ref, computed } from 'vue'
+import { defineStore } from 'pinia'
 
 export const useWebsocket = defineStore('websocket', () => {
-  const messages = ref<string>('');
+  const messages = ref<string>('')
 
-  const userName = ref<string>('');
-  const roomId = ref<string>('');
+  const userName = ref<string>('')
+  const roomId = ref<string>('')
 
   const players = ref<string[]>([])
 
-  const socket = ref<WebSocket | null>(null);
+  const socket = ref<WebSocket | null>(null)
 
   function joinNewSocket() {
     if (!socket.value) {
       socket.value = new WebSocket(
         `ws://localhost:8000?username=${userName.value}&room_id=${roomId.value}`
-      );
+      )
 
       socket.value.onopen = () => {
-        console.log('WebSocket connection established');
-      };
+        console.log('WebSocket connection established')
+      }
 
       socket.value.onerror = (error) => {
-        console.error('WebSocket encountered an error:', error);
-      };
+        console.error('WebSocket encountered an error:', error)
+      }
 
       socket.value.onmessage = (event) => {
-        console.log(event)
         try {
-          const message = JSON.parse(event.data);
-          messages.value = message;
-          if(message.players !== undefined) {
-            players.value = message.players
+          const message = JSON.parse(event.data)
+          if (message.status === 'user_connected' || message.status === 'connected') {
+            if (message.players !== undefined) {
+              players.value = message.players
+            } else {
+              players.value.push(message.user)
+            }
           } else {
-            players.value.push(message.user)
+            removePlayer(message.user);
           }
         } catch (parseError) {
-          console.error('Error parsing WebSocket message:', parseError);
+          console.error('Error parsing WebSocket message:', parseError)
         }
-      };
+      }
 
       socket.value.onclose = (event) => {
-        console.log("CLOSED PLAYER: ", event)
+        console.log('CLOSED PLAYER: ', event)
       }
     }
   }
 
+  function removePlayer(playerName: string) {
+    const index = players.value.indexOf(playerName);
+    if (index !== -1) {
+      players.value.splice(index, 1);
+    }
+  }
+  
   return {
     socket,
     joinNewSocket,
@@ -53,5 +62,5 @@ export const useWebsocket = defineStore('websocket', () => {
     userName,
     roomId,
     players
-  };
-});
+  }
+})
